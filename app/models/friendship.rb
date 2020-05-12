193285985dce3dbbfee93771da321cb1f812b28e
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-05-12 11:25:49 -0500

    Add accept method to confirm friendship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,5 +2,7 @@ class Friendship < ApplicationRecord
     belongs_to :user
     belongs_to :friend, class_name: "User"
 
-    
+    def accept
+        update_attribute(:confirmed, true)
+    end
 end
